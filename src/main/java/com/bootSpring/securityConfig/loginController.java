55945db4798308aebed9358e9a12890d2dccf29f
Administrator <13725581150@139.com>
2017-09-04 17:49:11 +0800

    Merge branch 'master' of https://github.com/MyGitByCai/springboot_mybatis_demo
    Conflicts:
    	src/main/java/com/bootSpring/securityConfig/loginController.java

--- src/main/java/com/bootSpring/securityConfig/loginController.java
+++ src/main/java/com/bootSpring/securityConfig/loginController.java
@@ -24,6 +24,7 @@ public class loginController {
 		User user = mapper.Select("1");
 		mod.addObject("user",user);
 		mod.setViewName("index");
+		System.out.println("eee");
 		System.out.println("ccc");
 		System.out.println("ooo");
 		System.out.println("dewdwefwaef");
